b45b82863f1063cfc7893f67eb115f9e4cf4c455
jn-prod <nicolas.jouanno@gmail.com>
2018-08-14 12:49:12 +0200

    js & css optimization with webpack

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,16 +1,13 @@
-
-// import '../img/bg-min.jpg'
-// import '@fortawesome/fontawesome'
-
 // js
 import './components/init'
 import './components/mediumish'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free'
 
 // fonts
 import '../fonts/Maximum_Impact.ttf'
 
 // css
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/style.css'
 // import '../css/main.scss'
-
-console.log('Hello, world!')
